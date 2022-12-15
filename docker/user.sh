edcb5e4fd2d8ce0a80ec5365d2a057a14e33709f
Trong Nhan Mai <trong.nhan.mai@oracle.com>
2022-12-15 14:07:11 +1000

    chore: add Dockerfile for deploying Macaron with ol9-slim
    Signed-off-by: Trong Nhan Mai <trong.nhan.mai@oracle.com>

--- docker/user.sh
+++ docker/user.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Copyright (c) 2022 - 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
-groupmod -o -g $USER_GID macaron
-usermod -o -g $USER_GID -u $USER_UID macaron
+# We update the GID and UID of the existing macaron user in the container
+# with the GID and UID of the current user in the host machine.
+# This will make the files created during the runtime of the container would have
+# the current user of the host machine as the owner.
+if [[ ! -z "$USER_GID" ]] && [[ ! -z "$USER_UID" ]];
+then
+    groupmod -o -g $USER_GID macaron
+    usermod -o -g $USER_GID -u $USER_UID macaron
+else
+    echo "Cannot find the GID and UID of the host machine's user. The output files generated could not be modifiable from the host machine."
+    echo "Consider providing the GID and UID via the env variables USER_GID and USER_UID respectively."
+fi
 
 # Prepare settings.xml because
 # We mount .m2 dir to the host machine
@@ -17,8 +27,20 @@ then
         mkdir -p $HOME/.m2
     fi
     cp $HOME/resources/settings.xml $HOME/.m2/
-    chown -R macaron:macaron $HOME/.m2
 fi
 
-COMMAND="cd /home/macaron && python3 -m macaron $@"
+# Prepare the output directory. The output directory will be already existed
+# if we mount from the host machine.
+if [[ ! -d "$HOME/output" ]];
+then
+    mkdir -p $HOME/output
+fi
+
+# The directory that could be mounted to the host machine file systems should
+# have the owner as the current user in the host machine.
+chown -R macaron:macaron $HOME/.m2
+chown -R macaron:macaron $HOME/output
+
+# Run the provided Macaron command with the user macaron.
+COMMAND="cd /home/macaron && . .venv/bin/activate && macaron $@"
 su macaron -m -c "$COMMAND"
