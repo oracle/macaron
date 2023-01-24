5fcb4f8db1ff7724a3fc04866e076cf1c556dadf
Alistair Michael <alistair.michael@oracle.com>
2023-01-24 15:59:55 +1000

    fix: copyright headers

--- src/macaron/slsa_analyzer/levels.py
+++ src/macaron/slsa_analyzer/levels.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """This module contains classes that handle the analysis of each SLSA levels."""
 
