d1a3fee9ca0fef8e90b55abe20f608b855be043f
Behnaz Hassanshahi <behnaz.hassanshahi@oracle.com>
2023-02-06 15:18:42 +1000

    build: remove paths outside the package (#28)
    Signed-off-by: behnazh-w <behnaz.hassanshahi@oracle.com>

--- src/macaron/__init__.py
+++ src/macaron/__init__.py
@@ -1,9 +1,14 @@
-# Copyright (c) 2022 - 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """This module initializes the necessary components for the macaron package."""
+
+import os
 
 # The version of this package. There's no comprehensive, official list of other
 # magic constants, so we stick with this one only for now. See also this conversation:
 # https://stackoverflow.com/questions/38344848/is-there-a-comprehensive-table-of-pythons-magic-constants
 __version__ = "0.0.0"
+
+# The path to the Macaron package.
+MACARON_PATH = os.path.dirname(os.path.abspath(__file__))
