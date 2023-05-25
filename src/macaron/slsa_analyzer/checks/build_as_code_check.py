65ed00050f982f2d7d66cd4d185a20c2efaad8d6
sophie-bates <sophie.bates@oracle.com>
2023-05-25 12:11:07 +1000

    feat: use Bayesian Network to perform confidence aggregation.

--- src/macaron/slsa_analyzer/checks/build_as_code_check.py
+++ src/macaron/slsa_analyzer/checks/build_as_code_check.py
@@ -3,14 +3,15 @@
 
 """This module contains the BuildAsCodeCheck class."""
 
-# import itertools
+from dataclasses import dataclass
+import itertools
 import logging
 import os
 from enum import Enum
 
-# from pgmpy.factors.discrete import TabularCPD
-# from pgmpy.inference import VariableElimination
-# from pgmpy.models.BayesianModel import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+from pgmpy.models.BayesianModel import BayesianNetwork
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql.sqltypes import String
 
@@ -45,41 +46,106 @@ class BuildAsCodeTable(CheckFactsTable, ORMBase):
     build_status_url: Mapped[str] = mapped_column(String, nullable=True)
 
 
-class EvidenceType(str, Enum):
-    """This class contains the types of evidence."""
-
-    FACT = "FACT"
-    EVIDENCE = "EVIDENCE"
-    CLUE = "CLUE"
-
-
+@dataclass
 class Evidence:
     """This class contains the evidence for a check."""
 
-    def __init__(
-        self,
-        name: str = "",
-        evidence_type: EvidenceType = EvidenceType.CLUE,
-        state: bool = False,
-        weight: float = 0,
-        norm_weight: float = 0,
-        description: str = "",
-        mutual_exclusion: list[str] = None,
-        children: list[str] = None,
-    ) -> None:
-        self.state = state
-        self.evidence_type = evidence_type
-        self.name = name
-        self.description = description
-        self.weight = weight
-        self.norm_weight = norm_weight
-        self.mutual_exclusion = mutual_exclusion
-        self.children = children
+    name: str = ""
+    # evidence_type: EvidenceType = EvidenceType.CLUE
+    state: bool = False
+    weight: float = 0
+    norm_weight: float = 0
+    description: str = ""
+    mutual_exclusion: list[str] = None
+    depends_on: list[str] = None
+    children: list[str] = None
 
-        # TODO: add optional dependencies on other factors, i.e. build tool, CI service.
+    # TODO: add optional dependencies on other factors, i.e. build tool, CI service.
 
     def __str__(self) -> str:
         return f"Name: {self.name}, weight: {self.weight}, state: {self.state}."
+
+
+class EvidenceGraph():
+    """This class contains the graph representation of evidence for a check."""
+
+    def __init__(self, check_id, evidence: dict[str, Evidence]) -> None:
+        self.nodes = [check_id, *evidence.keys()]
+        self.edges = []
+        self.evidence = evidence
+        self.check_id = check_id
+
+        # Construct edges list based on dependencies
+        # a -> b -> c == [(a, b), (b, c)]
+        for item in evidence.values():
+            if item.depends_on:
+                for dependent in item.depends_on:
+                    self.edges.append((dependent, item.name))
+
+        # Initialize the BN with the edges
+        self.model = BayesianNetwork(self.edges)
+
+        cpds: list[TabularCPD] = []
+
+        for item in self.evidence.values():
+            if not item.depends_on:
+                # Evidence is a root value, i.e., no parents and |values| == 2
+                cpds.append(TabularCPD(variable=item.name, variable_card=2, values=[[item.weight], [
+                    1 - item.weight]]))
+
+            else:
+                # Evidence is dependent on another piece of evidence.
+                true_states = []
+
+                # TODO: extend for multiple parents.
+                for parent in item.depends_on:
+                    tmp_weight = self.evidence[parent].weight
+                    true_states.extend([[item.weight, tmp_weight]])
+
+                lst = list(itertools.product(*true_states))
+
+                true_values = [sum(itemm) for itemm in lst]
+                false_values = [(1.0 - sum(itemm)) for itemm in lst]
+                values = [true_values, false_values]
+
+                cpds.append(TabularCPD(variable=item.name,
+                                       variable_card=2,
+                                       values=values,
+                                       evidence=item.depends_on,
+                                       evidence_card=[2 for parent in item.depends_on]))
+
+        for item in cpds:
+            self.model.add_cpds(item)
+
+    def validate_network(self) -> bool:
+        """Check that the network is a DAG with no cycles."""
+        return self.model.check_model()
+
+    def perform_variable_elimination(self):
+        """Perform Variable Elimination on the model."""
+        # Setup evidence to be used for inference.
+        collected_evidence = {}
+        for item in self.evidence.values():
+            collected_evidence[item.name] = item.state
+
+        # Remove the check_id node from the collected evidence, as we can't have evidence for node that we're inferring.
+        collected_evidence.pop(self.check_id)
+
+        # Initialize the class to perform variable elimination.
+        check_infer = VariableElimination(self.model)
+
+        # Perform variable elimination inference on the network using the found evidence.
+        query = check_infer.query(variables=[self.check_id], evidence=collected_evidence)
+
+        logger.info("Evidence: %s", collected_evidence)
+        logger.info("------------------ QUERY OUTPUT ------------------")
+        logger.info(query)
+
+        # TODO: investigate MAP query as potential option. MAP outputs most probable state of the 'variables', so will output True or False for build_as_code_check.
+        map_query = check_infer.map_query(variables=[self.check_id], evidence=collected_evidence)
+        logger.info(map_query)
+
+        return query
 
 
 class BuildAsCodeCheck(BaseCheck):
@@ -109,13 +175,19 @@ class BuildAsCodeCheck(BaseCheck):
 
         # Can define weight and type here, or leave as default.
         self.evidence = {
-            "ci_parsed": Evidence(name="ci_parsed", weight=0.2, description="CI files are parsed for this CI service"),
+            "ci_parsed": Evidence(name="ci_parsed", weight=0.65, description="CI files are parsed for this CI service"),
             "gha_deploy": Evidence(
-                name="gha_deploy", weight=0.7, description="Trusted GitHub Action used in CI workflow to deploy"
+                name="gha_deploy", weight=0.7, description="Trusted GitHub Action used in CI workflow to deploy", depends_on=["ci_parsed"]
             ),
             "cmd_deploy": Evidence(
-                name="cmd_deploy", weight=0.5, description="Bash command used to deploy", mutual_exclusion="gha_deploy"
+                name="cmd_deploy", weight=0.8, description="Bash command used to deploy", depends_on=["ci_parsed"]
             ),
+            "dist_has_wheel": Evidence(
+                name="dist_has_wheel", weight=0.75, description="/dist directory contains wheel.", depends_on=["cmd_deploy"]
+            ),
+            # TODO: decide how we'd want to deal with adding edges between the evidence and final outcome.
+            # Could automatically add edges between the leaves and the check node.
+            self.check_id: Evidence(name=self.check_id, weight=0.85, depends_on=["dist_has_wheel"])  # , "gha_deploy"])
         }
 
         self.check_confidence_threshold = 0.3
@@ -260,74 +332,70 @@ class BuildAsCodeCheck(BaseCheck):
                                         build_status_url=html_url,
                                     )
                                 ]
-                            self.evidence["gha_deploy"].state = True
-                            self.evidence["ci_parsed"].state = True
-                            self.evidence["cmd_deploy"].weight = 0.1
-                            print(workflow_name)
+                            # self.evidence["gha_deploy"].state = False
+                            # self.evidence["ci_parsed"].state = True
                             # return CheckResultType.PASSED
 
                 # TODO: handle mutual exclusion.
-                if self.evidence["gha_deploy"].state is False:
-                    for bash_cmd in ci_info["bash_commands"]:
-                        deploy_cmd = self._has_deploy_command(bash_cmd["commands"], build_tool)
-                        if deploy_cmd:
-                            # Get the permalink and HTML hyperlink tag of the CI file that triggered the bash command.
-                            trigger_link = ci_service.api_client.get_file_link(
-                                ctx.repo_full_name,
-                                ctx.commit_sha,
-                                ci_service.api_client.get_relative_path_of_workflow(
-                                    os.path.basename(bash_cmd["CI_path"])
-                                ),
-                            )
-                            # Get the permalink of the source file of the bash command.
-                            bash_source_link = ci_service.api_client.get_file_link(
-                                ctx.repo_full_name, ctx.commit_sha, bash_cmd["caller_path"]
-                            )
+                # if self.evidence["gha_deploy"].state is False:
+                for bash_cmd in ci_info["bash_commands"]:
+                    deploy_cmd = self._has_deploy_command(bash_cmd["commands"], build_tool)
+                    if deploy_cmd:
+                        # Get the permalink and HTML hyperlink tag of the CI file that triggered the bash command.
+                        trigger_link = ci_service.api_client.get_file_link(
+                            ctx.repo_full_name,
+                            ctx.commit_sha,
+                            ci_service.api_client.get_relative_path_of_workflow(
+                                os.path.basename(bash_cmd["CI_path"])
+                            ),
+                        )
+                        # Get the permalink of the source file of the bash command.
+                        bash_source_link = ci_service.api_client.get_file_link(
+                            ctx.repo_full_name, ctx.commit_sha, bash_cmd["caller_path"]
+                        )
 
-                            html_url = ci_service.has_latest_run_passed(
-                                ctx.repo_full_name,
-                                ctx.branch_name,
-                                ctx.commit_sha,
-                                ctx.commit_date,
-                                os.path.basename(bash_cmd["CI_path"]),
-                            )
+                        html_url = ci_service.has_latest_run_passed(
+                            ctx.repo_full_name,
+                            ctx.branch_name,
+                            ctx.commit_sha,
+                            ctx.commit_date,
+                            os.path.basename(bash_cmd["CI_path"]),
+                        )
 
-                            justification_cmd: list[str | dict[str, str]] = [
-                                {
-                                    f"The target repository uses build tool {build_tool.name} "
-                                    "to deploy": bash_source_link,
-                                    "The build is triggered by": trigger_link,
-                                },
-                                f"Deploy command: {deploy_cmd}",
-                                {"The status of the build can be seen at": html_url}
-                                if html_url
-                                else "However, could not find a passing workflow run.",
+                        justification_cmd: list[str | dict[str, str]] = [
+                            {
+                                f"The target repository uses build tool {build_tool.name} "
+                                "to deploy": bash_source_link,
+                                "The build is triggered by": trigger_link,
+                            },
+                            f"Deploy command: {deploy_cmd}",
+                            {"The status of the build can be seen at": html_url}
+                            if html_url
+                            else "However, could not find a passing workflow run.",
+                        ]
+                        check_result["justification"].extend(justification_cmd)
+                        if ctx.dynamic_data["is_inferred_prov"] and ci_info["provenances"]:
+                            predicate = ci_info["provenances"][0]["predicate"]
+                            predicate["buildType"] = f"Custom {ci_service.name}"
+                            predicate["builder"]["id"] = bash_source_link
+                            predicate["invocation"]["configSource"][
+                                "uri"
+                            ] = f"{ctx.remote_path}@refs/heads/{ctx.branch_name}"
+                            predicate["invocation"]["configSource"]["digest"]["sha1"] = ctx.commit_sha
+                            predicate["invocation"]["configSource"]["entryPoint"] = trigger_link
+                            predicate["metadata"]["buildInvocationId"] = html_url
+                            check_result["result_tables"] = [
+                                BuildAsCodeTable(
+                                    build_tool_name=build_tool.name,
+                                    ci_service_name=ci_service.name,
+                                    build_trigger=trigger_link,
+                                    deploy_command=deploy_cmd,
+                                    build_status_url=html_url,
+                                )
                             ]
-                            check_result["justification"].extend(justification_cmd)
-                            if ctx.dynamic_data["is_inferred_prov"] and ci_info["provenances"]:
-                                predicate = ci_info["provenances"][0]["predicate"]
-                                predicate["buildType"] = f"Custom {ci_service.name}"
-                                predicate["builder"]["id"] = bash_source_link
-                                predicate["invocation"]["configSource"][
-                                    "uri"
-                                ] = f"{ctx.remote_path}@refs/heads/{ctx.branch_name}"
-                                predicate["invocation"]["configSource"]["digest"]["sha1"] = ctx.commit_sha
-                                predicate["invocation"]["configSource"]["entryPoint"] = trigger_link
-                                predicate["metadata"]["buildInvocationId"] = html_url
-                                check_result["result_tables"] = [
-                                    BuildAsCodeTable(
-                                        build_tool_name=build_tool.name,
-                                        ci_service_name=ci_service.name,
-                                        build_trigger=trigger_link,
-                                        deploy_command=deploy_cmd,
-                                        build_status_url=html_url,
-                                    )
-                                ]
-                            self.evidence["ci_parsed"].state = True
-                            self.evidence["cmd_deploy"].state = True
-                            self.evidence["gha_deploy"].weight = 0.1
-
-                            # return CheckResultType.PASSED
+                        self.evidence["ci_parsed"].state = True
+                        self.evidence["cmd_deploy"].state = True
+                        self.evidence["dist_has_wheel"].state = True
 
                 # We currently don't parse these CI configuration files.
                 # We just look for a keyword for now.
@@ -368,31 +436,44 @@ class BuildAsCodeCheck(BaseCheck):
             found_evidence: list[Evidence] = []
             confidence_score = 0
 
+            print("EVIDENCE")
+            print(self.evidence)
             for item in self.evidence.values():
-                item.norm_weight = float(item.weight) / total_weight
                 if item.state:
-                    confidence_score += item.norm_weight
                     found_evidence.append(item)
 
-            # TODO: uncomment when using BN
-            # bayesian_network = EvidenceGraph(self.check_id, self.evidence)
-            # logger.info(bayesian_network.validate_network())
-            # check_infer = bayesian_network.perform_variable_elimination()
-            # logger.info(check_infer.map_query(variables=["build_as_code"], evidence=collected_evidence))
-
             # Processing evidence and confidence values
-            confidence_score_rounded = round(confidence_score, 4)
-            check_result["confidence_score"] = confidence_score_rounded
             evidence_list: list[str] = []
             for item in found_evidence:
                 evidence_list.append(item.description)
-                print(item.name, item.norm_weight)
+                print(item.name, item.weight)
             evidence_str = ", ".join(evidence_list)
 
             # TODO: ensure that this value hasn't been found.
-            evidence_importance = sorted(self.evidence.values(), key=lambda x: x.norm_weight, reverse=True)
+            evidence_importance = ""  # sorted(self.evidence.values(), key=lambda x: x.weight, reverse=True)
 
-            # Has the check passed (i.e., is the confidence score above the specified threshold?).
+            bayesian_network = EvidenceGraph(self.check_id, self.evidence)
+
+            # TODO: handle ValueError thrown from invalid network.
+            logger.info(bayesian_network.validate_network())
+
+            # Print the network
+            logger.info("------------------ Network setup ------------------")
+            logger.info("Nodes: %s", bayesian_network.model.nodes)
+            logger.info("Edges: %s", bayesian_network.model.edges)
+
+            for cpd in bayesian_network.model.cpds:
+                logger.info("\n")
+                logger.info(cpd)
+
+            query = bayesian_network.perform_variable_elimination()
+            confidence_score = query.values[0]
+
+            confidence_score_rounded = round(confidence_score, 4)
+
+            check_result["confidence_score"] = confidence_score_rounded
+
+            # Has the check passed(i.e., is the confidence score above the specified threshold?).
             if confidence_score > self.check_confidence_threshold:
                 message = f"The confidence score for this check is: {confidence_score_rounded}."
                 ev_msg = f"The evidence found: {evidence_str}."
@@ -403,7 +484,7 @@ class BuildAsCodeCheck(BaseCheck):
                 pass_msg = f"The target repository does not use {build_tool.name} to deploy. "
                 conf_msg = (
                     f"The confidence score for this check is {confidence_score_rounded}, "
-                    f"which is below the specified threshold of {self.check_confidence_threshold}"
+                    f"which is below the specified threshold of {self.check_confidence_threshold} "
                     "for this check."
                 )
                 improve_msg = (
