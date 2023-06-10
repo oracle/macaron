c7502c6d983bc32dac9eb94c9a871871a3b8206a
sophie-bates <sophie.bates@oracle.com>
2023-06-10 12:23:18 +1000

    chore: store workflow info object for each node in GitHub Actions callgraph
    Signed-off-by: sophie-bates <sophie.bates@oracle.com>

--- src/macaron/slsa_analyzer/ci_service/github_actions.py
+++ src/macaron/slsa_analyzer/ci_service/github_actions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 - 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """This module analyzes GitHub Actions CI."""
@@ -170,7 +170,7 @@ class GitHubActions(BaseCIService):
                             name=step["Exec"]["Uses"]["Value"],
                             node_type=GHWorkflowType.EXTERNAL,
                             source_path="",
-                            parsed_obj={},
+                            parsed_obj=step["Exec"],
                             caller_path=node.source_path,
                         )
                     )
