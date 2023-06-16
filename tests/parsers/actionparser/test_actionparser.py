5817c955d4e9aefca53bbee03ac5aebef6d36495
Behnaz Hassanshahi <behnaz.hassanshahi@oracle.com>
2023-06-16 19:09:15 +1000

    ci: update expected results for actionparser (#318)
    The new patch release of actionlint changes the schema of the serialized
    parsed object, failing our unit tests.
    
    See https://github.com/rhysd/actionlint/releases/tag/v1.6.25
    
    Signed-off-by: behnazh-w <behnaz.hassanshahi@oracle.com>

--- tests/parsers/actionparser/test_actionparser.py
+++ tests/parsers/actionparser/test_actionparser.py
@@ -1,61 +1,31 @@
-# Copyright (c) 2022 - 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """
 This module tests the GitHub Actions parser.
 """
 
-import json
 import os
 from pathlib import Path
 
+import pytest
+
+from macaron import MACARON_PATH
 from macaron.parsers.actionparser import parse
 
-from ...macaron_testcase import MacaronTestCase
+RESOURCES_DIR = Path(__file__).parent.joinpath("resources")
 
 
-class TestParsers(MacaronTestCase):
-    """Test the GitHub Actions parser."""
-
-    def test_actionparser_parse(self) -> None:
-        """Test parsing GH Actions workflows."""
-        resources_dir = Path(__file__).parent.joinpath("resources")
-
-        valid_results = []
-        valid_results.append(
-            parse(
-                os.path.join(resources_dir, "workflow_files", "codeql-analysis.yml"), str(MacaronTestCase.macaron_path)
-            )
-        )
-        valid_results.append(
-            parse(os.path.join(resources_dir, "workflow_files", "maven.yml"), str(MacaronTestCase.macaron_path))
-        )
-        valid_results.append(
-            parse(os.path.join(resources_dir, "workflow_files", "release.yaml"), str(MacaronTestCase.macaron_path))
-        )
-
-        expected_results = []
-        with open(os.path.join(resources_dir, "expected_results", "codeql-analysis.json"), encoding="utf8") as file:
-            expected_results.append(json.load(file))
-
-        with open(os.path.join(resources_dir, "expected_results", "maven.json"), encoding="utf8") as file:
-            expected_results.append(json.load(file))
-
-        with open(os.path.join(resources_dir, "expected_results", "release.json"), encoding="utf8") as file:
-            expected_results.append(json.load(file))
-
-        for index, valid_item in enumerate(valid_results):
-            assert valid_item == expected_results[index]
-
-        # Parse invalid workflows.
-        assert (
-            parse(os.path.join(resources_dir, "workflow_files", "invalid.yaml"), str(MacaronTestCase.macaron_path))
-            == {}
-        )
-        assert (
-            parse(
-                os.path.join(resources_dir, "workflow_files", "file_does_not_exist.yaml"),
-                str(MacaronTestCase.macaron_path),
-            )
-            == {}
-        )
+@pytest.mark.parametrize(
+    "workflow_path",
+    [
+        "codeql-analysis.yml",
+        "maven.yml",
+        "release.yml",
+        "invalid.yml",
+        "file_does_not_exist.yml",
+    ],
+)
+def test_actionparser_parse(snapshot: dict, workflow_path: str) -> None:
+    """Test parsing GH Actions workflows."""
+    assert parse(os.path.join(RESOURCES_DIR, "workflow_files", workflow_path), str(MACARON_PATH)) == snapshot
