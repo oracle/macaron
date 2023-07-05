4d96859a61da6b13300c739a5594a16814917091
Trong Nhan Mai <trong.nhan.mai@oracle.com>
2023-07-05 11:14:23 +1000

    chore: add tests for self-hosted gitlab
    Signed-off-by: Trong Nhan Mai <trong.nhan.mai@oracle.com>

--- tests/slsa_analyzer/git_service/test_gitlab.py
+++ tests/slsa_analyzer/git_service/test_gitlab.py
@@ -4,11 +4,14 @@
 """Tests for the GitLab git service."""
 
 import os
+from pathlib import Path
 from unittest import mock
 
 import pytest
 
-from macaron.slsa_analyzer.git_service.gitlab import PubliclyHostedGitLab
+from macaron.config.defaults import load_defaults
+from macaron.errors import ConfigurationError
+from macaron.slsa_analyzer.git_service.gitlab import PubliclyHostedGitLab, SelfHostedGitLab
 
 
 @pytest.mark.parametrize(
@@ -45,3 +48,66 @@ def test_construct_clone_url_with_token(repo_url: str, clone_url: str) -> None:
         gitlab = PubliclyHostedGitLab()
         gitlab.load_defaults()
         assert gitlab.construct_clone_url(repo_url) == clone_url
+
+
+@pytest.mark.parametrize(
+    ("user_config_input", "repo_url", "clone_url"),
+    [
+        pytest.param(
+            """
+            [git_service.gitlab.self_hosted]
+            domain = internal.gitlab.org
+            """,
+            "https://internal.gitlab.org/owner/repo.git",
+            "https://oauth2:abcxyz@internal.gitlab.org/owner/repo.git",
+            id="Self-hosted GitLab is set in user config.",
+        ),
+        pytest.param(
+            """
+            [git_service.gitlab.self_hosted]
+            domain = internal.gitlab.org
+            """,
+            "https://internal.gitlab.org/owner/repo",
+            "https://oauth2:abcxyz@internal.gitlab.org/owner/repo",
+            id="Self-hosted GitLab is set in user config.",
+        ),
+    ],
+)
+def test_construct_clone_url_for_self_hosted_gitlab(
+    user_config_input: str, repo_url: str, clone_url: str, tmp_path: Path
+) -> None:
+    """Test if the ``construct_clone_url`` method produces proper clone URLs with the access token."""
+    user_config_path = os.path.join(tmp_path, "config.ini")
+    with open(user_config_path, "w", encoding="utf-8") as user_config_file:
+        user_config_file.write(user_config_input)
+    # We don't have to worry about modifying the ``defaults`` object causing test
+    # pollution here, since we reload the ``defaults`` object before every test with the
+    # ``setup_test`` fixture.
+    load_defaults(user_config_path)
+
+    with mock.patch.dict(os.environ, {"MCN_SELF_HOSTED_GITLAB_TOKEN": "abcxyz"}):
+        gitlab = SelfHostedGitLab()
+        gitlab.load_defaults()
+        assert gitlab.construct_clone_url(repo_url) == clone_url
+
+
+def test_self_hosted_gitlab_without_env_set(tmp_path: Path) -> None:
+    """Test if the ``load_defaults`` method raises error if the required env variable is not set."""
+    user_config_input = """
+    [git_service.gitlab.self_hosted]
+    domain = internal.gitlab.org
+    """
+    user_config_path = os.path.join(tmp_path, "config.ini")
+    with open(user_config_path, "w", encoding="utf-8") as user_config_file:
+        user_config_file.write(user_config_input)
+
+    # We don't have to worry about modifying the ``defaults`` object causing test
+    # pollution here, since we reload the ``defaults`` object before every test with the
+    # ``setup_test`` fixture.
+    load_defaults(user_config_path)
+
+    with mock.patch.dict(os.environ, {"MCN_SELF_HOSTED_GITLAB_TOKEN": ""}):
+        gitlab = SelfHostedGitLab()
+
+        with pytest.raises(ConfigurationError):
+            gitlab.load_defaults()
