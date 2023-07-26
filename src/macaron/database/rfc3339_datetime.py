146d59f6370aa169f84e8cc6038b1c5c74d46ac8
Jens Tröger <jenstroeger@users.noreply.github.com>
2023-07-27 06:32:00 +1000

    fix(orm): serialize datetime object’s timezone instead of always coercing to UTC when persisting to the SQLite db (#381)
    Signed-off-by: Jens Troeger <jens.troeger@light-speed.de>

--- src/macaron/database/rfc3339_datetime.py
+++ src/macaron/database/rfc3339_datetime.py
@@ -4,7 +4,7 @@
 """This module implements SQLAlchemy type for converting date format to RFC3339 string representation."""
 
 import datetime
-from typing import Any, Optional
+from typing import Any
 
 from sqlalchemy import String, TypeDecorator
 
@@ -14,6 +14,7 @@ class RFC3339DateTime(TypeDecorator):  # pylint: disable=W0223
     SQLAlchemy column type to serialise datetime objects for SQLite in consistent format matching in-toto.
 
     https://docs.sqlalchemy.org/en/20/core/custom_types.html#store-timezone-aware-timestamps-as-timezone-naive-utc
+    https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#sqlalchemy.dialects.sqlite.DATETIME
     """
 
     # It is stored in the database as a string
@@ -23,28 +24,34 @@ class RFC3339DateTime(TypeDecorator):  # pylint: disable=W0223
     #: :meta private:
     cache_ok = True
 
-    def process_bind_param(self, value: Optional[Any], dialect: Any) -> None | str:
-        """Process when storing.
+    def process_bind_param(self, value: None | Any, dialect: Any) -> None | str:
+        """Process when storing a ``datetime`` object to the SQLite db.
+
+        If the timezone of the serialized ``datetime`` object is provided, this function preserves it. Otherwise,
+        if the provided ``datetime`` is a naive ``datetime`` object then UTC is added.
 
         value: None | datetime.datetime
             The value being stored
         """
-        result = None
-        if value is not None:
-            if not value.tzinfo:
-                raise TypeError("tzinfo is required")
-            value = value.astimezone(datetime.timezone.utc).replace(tzinfo=None)
-            result = value.isoformat(sep="T", timespec="seconds") + "Z"
-        return result
+        if value is None:
+            return None
+        if not isinstance(value, datetime.datetime):
+            raise TypeError("RFC3339DateTime type expects a datetime object")
+        if not value.tzinfo:
+            value = value.astimezone(datetime.timezone.utc)  # Consider coercing to host timezone.
+        return value.isoformat(timespec="seconds")
 
-    def process_result_value(self, value: Optional[Any], dialect: Any) -> None | datetime.datetime:
-        """Process when loading.
+    def process_result_value(self, value: None | str, dialect: Any) -> None | datetime.datetime:
+        """Process when loading a ``datetime`` object from the SQLite db.
+
+        If the deserialized ``datetime`` has a timezone then return it, otherwise add UTC as its timezone.
 
         value: None | str
             The value being loaded
         """
-        result = None
-        if value is not None:
-            result = datetime.datetime.fromisoformat(value)
-            result = result.astimezone(datetime.timezone.utc)
-        return result
+        if value is None:
+            return None
+        result = datetime.datetime.fromisoformat(value)
+        if result.tzinfo:
+            return result
+        return result.astimezone(datetime.timezone.utc)  # Consider coercing to host timezone.
