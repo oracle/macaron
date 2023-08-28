83f71a8f2969c93278b44574b9f72509260fa7c0
Nathan Nguyen <nathan.nguyen@oracle.com>
2023-08-28 17:39:27 +1000

    docs: fix class docstring for NamedTuple and dataclass classes
    Signed-off-by: Nathan Nguyen <nathan.nguyen@oracle.com>

--- src/macaron/slsa_analyzer/specs/package_registry_spec.py
+++ src/macaron/slsa_analyzer/specs/package_registry_spec.py
@@ -13,20 +13,11 @@ from macaron.slsa_analyzer.provenance.provenance import DownloadedProvenanceData
 
 @dataclass
 class PackageRegistryInfo:
-    """This class contains data for one package registry that is matched against a repository.
+    """This class contains data for one package registry that is matched against a repository."""
 
-    Attributes
-    ----------
-    build_tool : BaseBuildTool
-        The build tool matched against the repository.
-
-    package_registry : PackageRegistry
-        The package registry matched against the repository. This is dependent on the build tool detected.
-
-    provenances : list[IsProvenance]
-        The provenances matched against the current repo.
-    """
-
+    #: The build tool matched against the repository.
     build_tool: BaseBuildTool
+    #: The package registry matched against the repository. This is dependent on the build tool detected.
     package_registry: PackageRegistry
+    #: The provenances matched against the current repo.
     provenances: list[DownloadedProvenanceData] = field(default_factory=list)
