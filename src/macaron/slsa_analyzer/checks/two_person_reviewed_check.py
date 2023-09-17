544c531ff44809521f5688617442e0d0f18c1184
Yao-Wen Chang <changyaowen19980629@gmail.com>
2023-09-17 20:42:16 +1000

    fix: moving to graphql in two-person check
    Signed-off-by: Yao-Wen Chang <changyaowen19980629@gmail.com>

--- src/macaron/slsa_analyzer/checks/two_person_reviewed_check.py
+++ src/macaron/slsa_analyzer/checks/two_person_reviewed_check.py
@@ -6,6 +6,7 @@
 import logging
 import os
 
+import requests
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -15,7 +16,6 @@ from macaron.database.table_definitions import CheckFacts
 from macaron.slsa_analyzer.analyze_context import AnalyzeContext
 from macaron.slsa_analyzer.checks.base_check import BaseCheck
 from macaron.slsa_analyzer.checks.check_result import CheckResult, CheckResultType
-from macaron.slsa_analyzer.git_service.api_client import GhAPIClient
 from macaron.slsa_analyzer.registry import registry
 from macaron.slsa_analyzer.slsa_req import ReqName
 
@@ -39,7 +39,7 @@ class TwoPersonReviewedCheck(BaseCheck):
     def __init__(self) -> None:
         """Initiate the BuildScriptCheck instance."""
         check_id = "mcn_two_person_reviewed_1"
-        description = "Check whether the submitted code has been reviewd by two people."
+        description = "Check whether the merged pull requests has been reviewd and approved by at least one reviewer."
         depends_on: list[tuple[str, CheckResultType]] = []
         eval_reqs = [ReqName.TWO_PERSON_REVIEWED]
         super().__init__(
@@ -68,57 +68,80 @@ class TwoPersonReviewedCheck(BaseCheck):
         check_result["result_tables"] = [TwoPersonReviewedTable()]
         required_reviewers = defaults.get_list("check.two_person", "required_reviewers", fallback=[])
         logger.info("Reviewers number required: %s", {required_reviewers[0]})
-        api_client = GhAPIClient(
-            {
-                "headers": {
-                    "Accept": "application/vnd.github.v3+json",
-                    "Authorization": f"Bearer {os.environ.get('GITHUB_TOKEN')}",
-                    "X-GitHub-Api-Version": "2022-11-28",
-                },
-                "query": [],
+        # Your GitHub personal access token (replace with your own token)
+        token = os.getenv("GITHUB_TOKEN")
+        # GitHub GraphQL API endpoint
+        url = "https://api.github.com/graphql"
+        # Define the GraphQL query
+        query = """
+            query ($owner: String!, $name: String!, $cursor: String, $branch_name: String) {
+                repository(owner: $owner, name: $name) {
+                    pullRequests(first: 100, states: MERGED, after: $cursor, baseRefName: $branch_name) {
+                        totalCount
+                        pageInfo {
+                            hasNextPage
+                            endCursor
+                        }
+                        edges {
+                            node {
+                                reviewDecision
+                            }
+                        }
+                    }
+                }
             }
-        )
-        # Query the PR based on the branch name specified through user input tag.
-        pr_objects_list = api_client.list_pull_requests(
-            ctx.component.repository.full_name, ctx.component.repository.branch_name
-        )
-        merged_pr_num = 0  # Store the number of the PRs already been merged.
-        pass_num = 0  # If the pull request has been reviewed by 2 person, then increase the count.
-        for pr_object in pr_objects_list:
-            pr_number = pr_object["number"] if pr_object is not None and "number" in pr_object else None
-            if pr_number is None:
-                continue
-            pr_requester = pr_object.get("user", None).get("login", None)
-            if pr_requester is None:
-                continue
-            merged_at = pr_object["merged_at"] if pr_object is not None and "merged_at" in pr_object else None
-            if merged_at:  # Check the PR is merged or not.
-                merged_pr_num += 1
-                review_data = api_client.get_a_review(ctx.component.repository.full_name, str(pr_number))
-                reviewers = set()  # Use a set to avoid duplicates.
+        """
+        # Set up the HTTP headers with your token
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+        # Send the GraphQL query to GitHub API.
+        # 100 data per query.
+        approved_pr_num = 0
+        merged_pr_num = 0
+        has_next_page = True
+        end_cursor = None
 
-                for review in review_data:
-                    reviewer = review["user"]["login"]
-                    if reviewer != pr_requester:
-                        reviewers.add(reviewer)
-
-                if len(reviewers) >= int(required_reviewers[0]):
-                    pass_num += 1
+        while has_next_page:
+            variables = {
+                "owner": ctx.component.repository.owner,
+                "name": ctx.component.repository.name,
+                "cursor": end_cursor,
+                "branch_name": ctx.component.repository.branch_name,
+            }
+            response = requests.post(
+                url,
+                timeout=defaults.getint("requests", "timeout", fallback=10),
+                json={"query": query, "variables": variables},
+                headers=headers,
+            )  # nosec B113:request_without_timeout
+            if response.status_code == 200:
+                data = response.json()
+                merged_pr_num = data["data"]["repository"]["pullRequests"]["totalCount"]
+                for edge in data["data"]["repository"]["pullRequests"]["edges"]:
+                    review_decision = edge["node"]["reviewDecision"]
+                    if review_decision == "APPROVED":
+                        approved_pr_num += 1
+                has_next_page = data["data"]["repository"]["pullRequests"]["pageInfo"]["hasNextPage"]
+                end_cursor = data["data"]["repository"]["pullRequests"]["pageInfo"]["endCursor"]
+            else:
+                logger.error("%s, %s", response.status_code, response.text)
 
         logger.info(
             "%d pull requests have been reviewed by at least two person, and the pass rate is %d / %d",
-            pass_num,
-            pass_num,
+            approved_pr_num,
+            approved_pr_num,
             merged_pr_num,
         )
         check_result["justification"].extend(
             [
-                f"{str(pass_num)} pull requests have been reviewed by at least two person.",
-                f"The pass rate is {str(pass_num)} / {str(merged_pr_num)}",
+                f"{str(approved_pr_num)} pull requests have been reviewed by at least two person.",
+                f"The pass rate is {str(approved_pr_num)} / {str(merged_pr_num)}",
             ]
         )
-
-        if pass_num == merged_pr_num:
+        # print(f"[*] Two-person Reviewed in {duration} seconds")
+        if approved_pr_num == merged_pr_num:
             return CheckResultType.PASSED
         return CheckResultType.FAILED
 
