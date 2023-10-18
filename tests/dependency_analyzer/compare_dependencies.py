72ce3e3d4797e8c9e4191b12dddee8842e298982
Nathan Nguyen <nathan.nguyen@oracle.com>
2023-10-18 16:23:00 +1000

    chore: avoid running python dev scripts with system python (#520)
    Signed-off-by: Nathan Nguyen <nathan.nguyen@oracle.com>

--- tests/dependency_analyzer/compare_dependencies.py
+++ tests/dependency_analyzer/compare_dependencies.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-
-# Copyright (c) 2022 - 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 - 2023, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """
-This module checks the dependency analysis results against the expected outputs.
+This script checks the dependency analysis results against the expected outputs.
 """
 
 import json
