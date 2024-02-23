be6526260f23dae9d195a7f00c8a73c58734d5f8
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2024-02-23 17:06:23 +1000

    chore: improve test; fix defaults set issue in Java Repo Finder.
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

--- tests/repo_finder/test_repo_finder.py
+++ tests/repo_finder/test_repo_finder.py
@@ -2,13 +2,14 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """This module tests the repo finder."""
+import os
+from pathlib import Path
 
 import pytest
-from defusedxml.ElementTree import fromstring
 from packageurl import PackageURL
 
+from macaron.config.defaults import load_defaults
 from macaron.config.target_config import Configuration
-from macaron.repo_finder import repo_validator
 from macaron.repo_finder.repo_finder_java import JavaRepoFinder
 from macaron.slsa_analyzer.analyzer import Analyzer
 
@@ -74,14 +75,36 @@ def test_resolve_analysis_target(
     assert Analyzer.to_analysis_target(config, available_domains) == expect
 
 
-def test_pom_extraction_ordering() -> None:
+@pytest.mark.parametrize(
+    ("user_config_input", "expected"),
+    [
+        (
+            """
+                [repofinder.java]
+                repo_pom_paths =
+                    scm.connection
+                    scm.url
+                """,
+            ["scm:git:git@github.com:oracle-samples/macaron.git", "https://github.com/oracle/macaron"],
+        ),
+        (
+            """
+                [repofinder.java]
+                repo_pom_paths =
+                    scm.url
+                    scm.connection
+                """,
+            ["https://github.com/oracle/macaron", "scm:git:git@github.com:oracle-samples/macaron.git"],
+        ),
+    ],
+)
+def test_pom_extraction_ordering(tmp_path: Path, user_config_input: str, expected: list[str]) -> None:
     """Test the ordering of elements extracted from the POM is correct and maintained."""
     pom_text = """
     <project>
         <url>https://example.org</url>
         <scm>
             <connection>scm:git:git@github.com:oracle-samples/macaron.git</connection>
-            <developerConnection>scm:git:git@github.com:oracle/macaron.git</developerConnection>
             <url>https://github.com/oracle/macaron</url>
         </scm>
         <properties>
@@ -89,19 +112,12 @@ def test_pom_extraction_ordering() -> None:
         </properties>
     </project>
     """
-    pom = fromstring(pom_text)
+    user_config_path = os.path.join(tmp_path, "config.ini")
+    with open(user_config_path, "w", encoding="utf-8") as user_config_file:
+        user_config_file.write(user_config_input)
+    load_defaults(user_config_path)
+
     repo_finder = JavaRepoFinder()
 
     # Retrieve SCM from POM.
-    connection_urls = repo_finder._find_scm(pom, ["scm.connection", "scm.url"])  # pylint: disable=W0212
-    assert connection_urls
-    connection_url = repo_validator.find_valid_repository_url(connection_urls)
-    assert connection_url
-
-    urls = repo_finder._find_scm(pom, ["scm.url", "scm.connection"])  # pylint: disable=W0212
-    assert urls
-    url = repo_validator.find_valid_repository_url(urls)
-    assert url
-
-    # Ensure found URLs differ.
-    assert connection_url != url
+    assert expected == repo_finder._read_pom(pom_text)  # pylint: disable=W0212
