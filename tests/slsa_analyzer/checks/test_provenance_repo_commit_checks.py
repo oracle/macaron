23be3f7cb874d6e2f4ea56d6d0068295132f07bf
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2024-05-14 15:42:28 +1000

    chore: update context attributes in test
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

--- tests/slsa_analyzer/checks/test_provenance_repo_commit_checks.py
+++ tests/slsa_analyzer/checks/test_provenance_repo_commit_checks.py
@@ -51,8 +51,8 @@ def test_provenance_repo_commit_checks_pass(
 ) -> None:
     """Test combinations of Repository objects and provenance strings against check."""
     context = _prepare_context(macaron_path, repository)
-    context.dynamic_data["provenance_repo"] = repo_url
-    context.dynamic_data["provenance_commit"] = commit_digest
+    context.dynamic_data["provenance_repo_url"] = repo_url
+    context.dynamic_data["provenance_commit_digest"] = commit_digest
 
     # Check Repo
     repo_result = _perform_check_assert_result_return_result(
