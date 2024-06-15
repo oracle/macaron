6599f500687c01eb30815a0dc7f12fd31fdc7a3c
Nathan Nguyen <nathan.nguyen@oracle.com>
2024-06-16 03:16:52 +1000

    chore: convert case gitlab_tinyMediaManager_purl
    Signed-off-by: Nathan Nguyen <nathan.nguyen@oracle.com>

--- scripts/dev_scripts/integration_tests.sh
+++ scripts/dev_scripts/integration_tests.sh
@@ -283,15 +283,6 @@ run_macaron_clean $ANALYZE -purl pkg:maven/com.google.guava/guava@32.1.2-jre?typ
 
 $RUN_POLICY -d $DB -f $OUTPUT_POLICY || log_fail
 
-echo -e "\n----------------------------------------------------------------------------------"
-echo "org.tinymediamanager/tinyMediaManager: Analyzing the purl with a version, and a provided repo with no commit."
-echo -e "----------------------------------------------------------------------------------\n"
-OUTPUT_POLICY=$WORKSPACE/tests/e2e/expected_results/purl/org_tinymediamanager/tinyMediaManager.dl
-run_macaron_clean $ANALYZE -purl pkg:maven/org.tinymediamanager/tinyMediaManager@4.3.13 -rp https://gitlab.com/tinyMediaManager/tinyMediaManager --skip-deps || log_fail
-
-$RUN_POLICY -d $DB -f $OUTPUT_POLICY || log_fail
-
-
 # Running Macaron using local paths.
 echo -e "\n=================================================================================="
 echo "Run integration tests with local paths for apache/maven..."
