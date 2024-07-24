1fede15302c72c61ce7fba35892e5bc6916e4873
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2024-07-24 13:40:05 +1000

    chore: add verify check to more test cases
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

--- src/macaron/slsa_analyzer/checks/provenance_verified_check.py
+++ src/macaron/slsa_analyzer/checks/provenance_verified_check.py
@@ -79,6 +79,15 @@ class ProvenanceVerifiedCheck(BaseCheck):
         if predicate:
             build_type = json_extract(predicate, ["buildType"], str)
 
+        if build_type and build_type == "https://github.com/slsa-framework/slsa-github-generator/generic@v1":
+            # Provenance is created by the SLSA GitHub generator and therefore verified.
+            return CheckResultData(
+                result_tables=[
+                    ProvenanceVerifiedFacts(build_level=3, build_type=build_type, confidence=Confidence.HIGH)
+                ],
+                result_type=CheckResultType.PASSED,
+            )
+
         if not ctx.dynamic_data["provenance_verified"]:
             # Provenance is not verified.
             return CheckResultData(
@@ -92,20 +101,11 @@ class ProvenanceVerifiedCheck(BaseCheck):
                 result_type=CheckResultType.FAILED,
             )
 
-        if not (build_type and build_type == "https://github.com/slsa-framework/slsa-github-generator/generic@v1"):
-            # Provenance is a verified but not a SLSA GitHub Generator kind.
-            return CheckResultData(
-                result_tables=[
-                    ProvenanceVerifiedFacts(build_level=2, build_type=build_type, confidence=Confidence.HIGH)
-                ],
-                result_type=CheckResultType.PASSED,
-            )
-
-        # Provenance is verified and from the SLSA GitHub Generator.
+        # Provenance is verified.
         return CheckResultData(
             result_tables=[
                 ProvenanceVerifiedFacts(
-                    build_level=3,
+                    build_level=2,
                     build_type=build_type,
                     confidence=Confidence.HIGH,
                 )
