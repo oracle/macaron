4e098cb1af2c1233b8f0dc92ee8bc540e173c122
Trong Nhan Mai <trong.nhan.mai@oracle.com>
2024-08-16 14:55:02 +1000

    chore: remove get group id using gradlew logic

--- tests/slsa_analyzer/build_tool/test_gradle.py
+++ tests/slsa_analyzer/build_tool/test_gradle.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import pytest
 
 from macaron.code_analyzer.call_graph import BaseNode
-from macaron.config.defaults import load_defaults
 from macaron.slsa_analyzer.build_tool.base_build_tool import BuildToolCommand
 from macaron.slsa_analyzer.build_tool.gradle import Gradle
 from macaron.slsa_analyzer.build_tool.language import BuildLanguage
@@ -41,81 +40,6 @@ def test_gradle_build_tool(gradle_tool: Gradle, macaron_path: str, mock_repo: st
     base_dir = Path(__file__).parent
     ctx = prepare_repo_for_testing(mock_repo, macaron_path, base_dir)
     assert gradle_tool.is_detected(ctx.component.repository.fs_path) == expected_value
-
-
-def test_get_group_ids_nested_projects(tmp_path: Path, gradle_tool: Gradle) -> None:
-    """Test the ``get_group_ids`` method in case there are Gradle projects nested under a root project.
-
-    In this case, we should only obtain the group id of the root project, making the assumption
-    that all subprojects under it have the same group id.
-
-    This is consistent with the behavior of the ``get_build_dirs`` method.
-    """
-    repo_dir = tmp_path.joinpath("repo")
-    subproject_a_dir = repo_dir.joinpath("subprojecta")
-    subproject_b_dir = repo_dir.joinpath("subprojectb")
-
-    subproject_a_dir.mkdir(parents=True)
-    subproject_b_dir.mkdir(parents=True)
-
-    with open(repo_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut"')
-    with open(subproject_a_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut.foo"')
-    with open(subproject_b_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut.bar"')
-
-    assert set(gradle_tool.get_group_ids(str(repo_dir))) == {"io.micronaut"}
-
-
-def test_get_group_ids_separate_projects(tmp_path: Path, gradle_tool: Gradle) -> None:
-    """Test the ``get_group_ids`` method in case there are multiple separate Gradle projects in a repo.
-
-    "Separate projects" means they are in different directories in the repo.
-    """
-    repo_dir = tmp_path.joinpath("repo")
-
-    project_a_dir = repo_dir.joinpath("subprojecta")
-    project_b_dir = repo_dir.joinpath("subprojectb")
-
-    project_a_dir.mkdir(parents=True)
-    project_b_dir.mkdir(parents=True)
-
-    with open(project_a_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut.foo"')
-    with open(project_b_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut.bar"')
-
-    assert set(gradle_tool.get_group_ids(str(repo_dir))) == {
-        "io.micronaut.foo",
-        "io.micronaut.bar",
-    }
-
-
-@pytest.mark.parametrize(("timeout", "expected"), [("0", set()), ("invalid", {"io.micronaut"})])
-def test_get_group_ids_timeout(tmp_path: Path, gradle_tool: Gradle, timeout: str, expected: set) -> None:
-    """Test the timeout configuration on ``get_group_ids`` method."""
-    repo_dir = tmp_path.joinpath("repo")
-    repo_dir.mkdir()
-
-    with open(repo_dir.joinpath("build.gradle"), "w", encoding="utf-8") as file:
-        file.write('group = "io.micronaut"')
-
-    user_config_path = str(tmp_path.joinpath("config.ini"))
-    user_config_input = f"""
-        [builder.gradle.runtime]
-        build_timeout = {timeout}
-    """
-    with open(user_config_path, "w", encoding="utf-8") as user_config_file:
-        user_config_file.write(user_config_input)
-
-    # We don't have to worry about modifying the ``defaults`` object causing test
-    # pollution here, since we reload the ``defaults`` object before every test with the
-    # ``setup_test`` fixture.
-    load_defaults(user_config_path)
-    gradle_tool.load_defaults()
-
-    assert set(gradle_tool.get_group_ids(str(repo_dir))) == expected
 
 
 @pytest.mark.parametrize(
