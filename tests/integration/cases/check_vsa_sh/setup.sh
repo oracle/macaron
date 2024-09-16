c754882643b9a80c5d5963b51c3180b1fcfa73df
Behnaz Hassanshahi <behnaz.hassanshahi@oracle.com>
2024-09-16 10:29:21 +1000

    feat: add a script to check VSA (#858)
    This PR adds the check_vsa.sh script, which takes an artifact, VSA, and the package URL as the artifact identifier and reports whether the verification result has been successful.
    
    This PR also adds a tutorial for the VSAs published for the Graal Development Kit (GDK) artifacts.
    
    The integration test checks micronaut-core@4.5.3 with a manually generated VSA as well as micronaut-core@4.6.5 whose VSA is published on Oracle Maven.
    
    Signed-off-by: behnazh-w <behnaz.hassanshahi@oracle.com>

---
+++ tests/integration/cases/check_vsa_sh/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright (c) 2024 - 2024, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
+
+mkdir ./output
+cp ../../../../scripts/release_scripts/check_vsa.sh ./output/check_vsa.sh
