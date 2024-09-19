1661b839fbe4970de2105f2f9fd159f83a6eef7f
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2024-09-19 11:54:31 +1000

    chore: move pom parser to parsers module (#863)
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

---
+++ src/macaron/parsers/pomparser.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2024 - 2024, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
+
+"""This module contains the parser for POM files."""
+import logging
+from xml.etree.ElementTree import Element  # nosec
+
+import defusedxml.ElementTree
+from defusedxml.ElementTree import fromstring
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def parse_pom_string(pom_string: str) -> Element | None:
+    """
+    Parse the passed POM string using defusedxml.
+
+    Parameters
+    ----------
+    pom_string : str
+        The contents of a POM file as a string.
+
+    Returns
+    -------
+    Element | None
+        The parsed element representing the POM's XML hierarchy.
+    """
+    try:
+        # Stored here first to help with type checking.
+        pom: Element = fromstring(pom_string)
+        return pom
+    except defusedxml.ElementTree.ParseError as error:
+        logger.debug("Failed to parse XML: %s", error)
+        return None
