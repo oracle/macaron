5c490f9ef575eea4ac78508f6cc8dafdf8e2cb64
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2024-09-25 13:53:00 +1000

    chore: improve commit finder accuracy (#862)
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

--- tests/repo_finder/test_commit_finder.py
+++ tests/repo_finder/test_commit_finder.py
@@ -126,10 +126,10 @@ def test_commit_finder() -> None:
     git_obj.repo.create_tag(tag_tree_version, ref=tree)
 
     # Add a new tag with an associated commit. This is the Japanese character for 'snow'.
-    bad_version = "雪"
-    git_obj.repo.create_tag(bad_version, commit_0.hexsha)
+    unicode_version = "雪"
+    git_obj.repo.create_tag(unicode_version, commit_0.hexsha)
 
-    # Create a more proper tag on the same commit.
+    # Create a more typical tag on the same commit.
     tag_version = "2.3.4"
     git_obj.repo.create_tag(tag_version, commit_0.hexsha)
 
@@ -140,14 +140,14 @@ def test_commit_finder() -> None:
     git_obj.repo.create_tag(f"{tag_version_2}_DEV_RC1_RELEASE", ref=empty_commit.hexsha)
     git_obj.repo.create_tag(f"rel/prefix_name-{tag_version}", ref=empty_commit.hexsha)
 
-    # Version that fails to create a pattern.
-    assert not commit_finder.find_commit(git_obj, PackageURL.from_string(f"pkg:maven/apache/maven@{bad_version}"))
-
     # Version with a suffix and no matching tag.
     assert not commit_finder.find_commit(git_obj, PackageURL.from_string("pkg:maven/apache/maven@1-JRE"))
 
     # Version with only one digit and no matching tag.
     assert not commit_finder.find_commit(git_obj, PackageURL.from_string("pkg:maven/apache/maven@1"))
+
+    # Unicode version.
+    assert commit_finder.find_commit(git_obj, PackageURL.from_string(f"pkg:maven/apache/maven@{unicode_version}"))
 
     # Valid repository PURL.
     digest = commit_finder.find_commit(git_obj, PackageURL.from_string(f"pkg:github/apache/maven@{commit_0.hexsha}"))
@@ -230,7 +230,7 @@ def test_version_to_tag_matching(_data: DataObject) -> None:  # noqa: PT019
     sep = "[^a-z0-9]"
     tag_pattern = (
         "(?P<prefix_0>(?:[a-z].*(?:[a-z0-9][a-z][0-9]+|[0-9][a-z]|[a-z]{2}))|[a-z]{2})?("
-        "?P<prefix_sep_0>(?:(?:(?<![0-9a-z])[vrc])|(?:[^0-9a-z][vrc])|[^0-9a-z])(?:[^0-9a-z])?)?"
+        "?P<prefix_sep_0>(?:(?:(?<![0-9a-z])[vrc])|(?:[^0-9a-z][vrc])|[^0-9a-z])(?:[^0-9a-z])?)"
     )
     for count, part in enumerate(parts):
         if count > 0:
@@ -242,5 +242,7 @@ def test_version_to_tag_matching(_data: DataObject) -> None:  # noqa: PT019
     for _ in range(TAG_ITERATIONS):
         tag = _data.draw(hypothesis.strategies.from_regex(compiled_pattern, fullmatch=True))
         # Perform the match.
-        match = pattern.match(tag)
-        assert match
+        pattern.match(tag)
+
+        # We do not assert that the match succeeded as the patterns here no longer reflect the state of the commit
+        # finder. This test is left in place to check for exceptions and potential ReDoS bugs.
