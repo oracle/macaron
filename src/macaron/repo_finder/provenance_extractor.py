b1f49f7415257b9c0824ad802f13f5bccc692ac0
Ben Selwyn-Smith <benselwynsmith@googlemail.com>
2025-02-15 01:01:00 +1000

    chore: remove old provenance scripts and trigger the pylint bug
    Signed-off-by: Ben Selwyn-Smith <benselwynsmith@googlemail.com>

--- src/macaron/repo_finder/provenance_extractor.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2024 - 2025, Oracle and/or its affiliates. All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
-
-"""This module contains methods for extracting repository and commit metadata from provenance files."""
