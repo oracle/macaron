5a896393780f897e84befe9ca28c09161a93378f
Trong Nhan Mai <trong.nhan.mai@oracle.com>
2025-08-07 00:01:09 +1000

    feat: always prioritize jdk version obtained from JAR from maven central

--- tests/build_spec_generator/reproducible_central/test_reproducible_central.py
+++ tests/build_spec_generator/reproducible_central/test_reproducible_central.py
@@ -8,7 +8,10 @@ import pytest
 from macaron.build_spec_generator.reproducible_central.reproducible_central import (
     _get_build_command_sequence,
     _get_extra_comments,
+    _get_rc_build_tool_name,
+    _ReproducibleCentralBuildToolName,
 )
+from macaron.slsa_analyzer.checks.build_tool_check import BuildToolFacts
 
 
 @pytest.mark.parametrize(
@@ -62,3 +65,66 @@ def test_get_build_command_sequence(
 ) -> None:
     """Test the _get_build_command_sequence function."""
     assert _get_build_command_sequence(cmds_sequence) == expected
+
+
+@pytest.mark.parametrize(
+    ("build_tool_facts", "expected"),
+    [
+        pytest.param(
+            [
+                BuildToolFacts(
+                    language="python",
+                    build_tool_name="pip",
+                )
+            ],
+            None,
+            id="python_is_not_supported_for_rc",
+        ),
+        pytest.param(
+            [
+                BuildToolFacts(
+                    language="java",
+                    build_tool_name="gradle",
+                )
+            ],
+            _ReproducibleCentralBuildToolName.GRADLE,
+            id="build_tool_gradle",
+        ),
+        pytest.param(
+            [
+                BuildToolFacts(
+                    language="java",
+                    build_tool_name="maven",
+                )
+            ],
+            _ReproducibleCentralBuildToolName.MAVEN,
+            id="build_tool_maven",
+        ),
+        pytest.param(
+            [
+                BuildToolFacts(
+                    language="not_java",
+                    build_tool_name="maven",
+                )
+            ],
+            None,
+            id="java_is_the_only_supported_language",
+        ),
+        pytest.param(
+            [
+                BuildToolFacts(
+                    language="java",
+                    build_tool_name="some_java_build_tool",
+                )
+            ],
+            None,
+            id="test_unsupported_java_build_tool",
+        ),
+    ],
+)
+def test_get_rc_build_tool_name(
+    build_tool_facts: list[BuildToolFacts],
+    expected: _ReproducibleCentralBuildToolName | None,
+) -> None:
+    """Test the _get_rc_build_tool_name function."""
+    assert _get_rc_build_tool_name(build_tool_facts) == expected
