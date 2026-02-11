95c9010c6c65cf9787f321cc21fab137424f8162
behnazh-w <behnaz.hassanshahi@oracle.com>
2026-02-11 16:14:25 +1000

    chore(deps): update Python dependencies
    Signed-off-by: behnazh-w <behnaz.hassanshahi@oracle.com>

--- tests/integration/run.py
+++ tests/integration/run.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 - 2025, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2024 - 2026, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 """Integration test utility."""
@@ -1220,7 +1220,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     path = shutil.which(args.macaron)
     if path is None:
-        logger.error("'%s' is not a command.")
+        logger.error("'%s' is not a command.", args.macaron)
         return 1
     macaron_cmd = os.path.abspath(path)
 
