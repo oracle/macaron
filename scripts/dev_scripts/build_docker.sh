b3a61b98c15fc5fc93622da7685e2a212ff40ed5
Jens Tröger <jenstroeger@users.noreply.github.com>
2026-04-20 15:53:49 +1000

    build: make the Docker image building reproducible by using Macaron’s pinned and hashed requirements.txt (#1377)
    Following up on comment #1358 (comment), this change makes the Docker image building reproducible by using Macaron’s pinned and hashed requirements.txt.
    
    Signed-off-by: Jens Troeger <jens.troeger@light-speed.de>

--- scripts/dev_scripts/build_docker.sh
+++ scripts/dev_scripts/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2023 - 2023, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2023 - 2026, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 # This script is used to build the Docker image. The built image will always be tagged with `latest`.
@@ -10,6 +10,10 @@
 #   $ RELEASE_TAG: The additionally release tag to tag the final built image. If it's empty, the built image
 #       will be additionally tagged with `test`.
 
+if [ "$#" -ne 3 ];
+then
+    echo "Required arguments are missing" && exit 1
+fi
 IMAGE_NAME=$1
 WORKSPACE=$2
 RELEASE_TAG=$3
@@ -17,29 +21,35 @@ RELEASE_TAG=$3
 DIST_PATH="${WORKSPACE}/dist"
 REPO_PATH="${WORKSPACE}"
 
-WHEEL_PATH=$(find "$DIST_PATH" -depth -type f -name 'macaron-*.whl' | head -n 1)
-if [[ -z "${WHEEL_PATH}" ]];
-then
-    echo "Unable to find Macaron wheel file in ${DIST_PATH}."
+SIMPLE_INDEX_PATH=$(find "${DIST_PATH}" -depth -type f -name 'macaron-*-pep503-simple-index.tar' | head -n 1)
+if [[ -z "${SIMPLE_INDEX_PATH}" ]]; then
+    echo "Unable to find Macaron Simple Index in ${DIST_PATH}."
     exit 1
 fi
+SIMPLE_INDEX_PATH=$(realpath --relative-to "${REPO_PATH}" "${SIMPLE_INDEX_PATH}")
 
-# We need to use the relative path so that it works in the docker context.
-WHEEL_PATH=$(realpath --relative-to="$REPO_PATH" "$WHEEL_PATH")
+REQUIREMENTS_PATH=$(find "${DIST_PATH}" -depth -type f -name 'macaron-*-requirements.txt' | head -n 1)
+if [[ -z "${REQUIREMENTS_PATH}" ]]; then
+    echo "Unable to find Macaron requirements.txt in ${DIST_PATH}."
+    exit 1
+fi
+REQUIREMENTS_PATH=$(realpath --relative-to "${REPO_PATH}" "${REQUIREMENTS_PATH}")
 
 if [[ -z "${RELEASE_TAG}" ]];
 then
     docker build \
-        -t "${IMAGE_NAME}:latest" \
-        -t "${IMAGE_NAME}:test" \
-        --build-arg WHEEL_PATH="${WHEEL_PATH}" \
-        -f "${REPO_PATH}/docker/Dockerfile.final" "${REPO_PATH}"
+        --tag "${IMAGE_NAME}:latest" \
+        --tag "${IMAGE_NAME}:test" \
+        --build-arg SIMPLE_INDEX_PATH="${SIMPLE_INDEX_PATH}" \
+        --build-arg REQUIREMENTS_PATH="${REQUIREMENTS_PATH}" \
+        --file "${REPO_PATH}/docker/Dockerfile.final" "${REPO_PATH}"
 else
     docker build \
-        -t "${IMAGE_NAME}:latest" \
-        -t "${IMAGE_NAME}:${RELEASE_TAG}" \
-        --build-arg WHEEL_PATH="${WHEEL_PATH}" \
-        -f "${REPO_PATH}/docker/Dockerfile.final" "$REPO_PATH"
+        --tag "${IMAGE_NAME}:latest" \
+        --tag "${IMAGE_NAME}:${RELEASE_TAG}" \
+        --build-arg SIMPLE_INDEX_PATH="${SIMPLE_INDEX_PATH}" \
+        --build-arg REQUIREMENTS_PATH="${REQUIREMENTS_PATH}" \
+        --file "${REPO_PATH}/docker/Dockerfile.final" "$REPO_PATH"
 fi
 
 exit 0
