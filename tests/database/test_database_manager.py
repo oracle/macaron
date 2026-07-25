a393b3fbe508abb57458a1fa8d835cbe845f2799
behnazh-w <behnaz.hassanshahi@oracle.com>
2026-07-26 09:37:57 +1000

    chore: address PR feedback by using closing()
    Signed-off-by: behnazh-w <behnaz.hassanshahi@oracle.com>

--- tests/database/test_database_manager.py
+++ tests/database/test_database_manager.py
@@ -6,6 +6,7 @@
 import os
 import sqlite3
 from collections.abc import Iterable
+from contextlib import closing
 from pathlib import Path
 
 import pytest
@@ -37,13 +38,11 @@ def db_man() -> Iterable:
     """Set up the database and ensure it is empty."""
     db_manager = DatabaseManager(DB_PATH, base=Base)
     con = sqlite3.connect(DB_PATH)
-    try:
-        with con:
-            con.execute("drop table if exists _test_orm_table;")
-            con.execute("drop view if exists test_orm_table;")
-            con.commit()
-    finally:
-        con.close()
+    with closing(con), con:
+        con.execute("drop table if exists _test_orm_table;")
+        con.execute("drop view if exists test_orm_table;")
+        con.commit()
+
     yield db_manager
     db_manager.engine.dispose()
     os.remove(DB_PATH)
@@ -70,10 +69,7 @@ def test_orm_mapping(
 
     query = "select * from _test_orm_table;"
     con = sqlite3.connect(DB_PATH)
-    try:
-        with con:
-            cursor = con.execute(query)
-            rows = cursor.fetchall()
-            assert (str(rows) == str([(identifier, test_value)])) == expect
-    finally:
-        con.close()
+    with closing(con), con:
+        cursor = con.execute(query)
+        rows = cursor.fetchall()
+        assert (str(rows) == str([(identifier, test_value)])) == expect
